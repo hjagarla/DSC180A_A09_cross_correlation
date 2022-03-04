b64d4a4fd911882fab91ef4d53f501831ad3b84b
kel-kong <kkong@ucsd.edu>
2022-03-03 22:41:30 -0800

    add try catch

--- run.py
+++ run.py
@@ -60,11 +60,14 @@ def main(targets):
     if 'test' in targets:
         temp = template(temp_path)
         model = correlation(technique, threshold_type, threshold_const, threshold_min, bi_dir, window_size)
-        for clip_path in clip_paths:
-            audio = load_audio(clip_path)
-            tf_audio = spectrogram(audio)
-            output = test(clip_path, tf_audio, temp, audio, model)
-            print(output)
+        try:
+            for clip_path in clip_paths:
+                audio = load_audio(clip_path)
+                tf_audio = spectrogram(audio)
+                output = test(clip_path, tf_audio, temp, audio, model)
+                print(output)
+        except:
+            pass
 
 if __name__ == '__main__':
     # run via:

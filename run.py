ad97048f8c5c6de5a39b56528c3401156e7d1131
kel-kong <kkong@ucsd.edu>
2022-03-06 14:03:09 -0800

    error

--- run.py
+++ run.py
@@ -64,13 +64,10 @@ def main(targets):
         temp = template(temp_path)
         model = correlation(technique, threshold_type, threshold_const, threshold_min, bi_dir, window_size)
         for clip_path in clip_paths:
-            try:
-                audio = load_audio(clip_path)
-                tf_audio = spectrogram(audio)
-                output = test(clip_path, tf_audio, temp, audio, model)
-                print(output)
-            except:
-                pass
+            audio = load_audio(clip_path)
+            tf_audio = spectrogram(audio)
+            output = test(clip_path, tf_audio, temp, audio, model)
+            print(output)
 
 
 if __name__ == '__main__':

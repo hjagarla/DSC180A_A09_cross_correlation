07669a5e55760fb9be19baae69052328ec31fefb
kel-kong <kkong@ucsd.edu>
2022-03-07 15:38:57 -0800

    ignore

--- run.py
+++ run.py
@@ -64,11 +64,11 @@ def main(targets):
         temp = template(temp_path)
         model = correlation(technique, threshold_type, threshold_const, threshold_min, bi_dir, window_size)
         for clip_path in clip_paths[0:10]:
+            print(clip_path)
             try:
                 audio = load_audio(clip_path)
                 tf_audio = spectrogram(audio)
                 output = test(clip_path, tf_audio, temp, audio, model)
-                print(clip_path)
             except:
                 pass
 
